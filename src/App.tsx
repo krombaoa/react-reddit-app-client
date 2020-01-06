eed4f4546199f4da6048fb0a9fe1c4178dcb9b11
krombaoa <krombaoa@joinpiggy.com>
2020-01-06 17:49:30 +0800

    sample

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'semantic-ui-react'
 
 const App: React.FC = () => {
+
+  useEffect(() => {
+    axios.get(`https://www.reddit.com/new.json`)
+      .then(res => {
+        console.log(res.data.data.children);
+      })
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+
+    </React.Fragment>
   );
 }
 
